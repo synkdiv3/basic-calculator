7136d8692f37a5a5178f9e9d3ffc7091359de8d7
synkdiv3 <117483732+synkdiv3@users.noreply.github.com>
2023-01-18 15:06:39 -0300

    Create index.html
    Index and assets created

---
+++ assets/calculator.js
@@ -0,0 +1,27 @@
+(function() {
+    let display = document.querySelector('.display')
+    let buttons = document.querySelectorAll('.button')
+    let clear = document.querySelector('.clear')
+    let equal = document.querySelector('.equal')
+
+    buttons.forEach(function(button){
+        button.addEventListener('click', function(e){
+            let value = e.target.dataset.num;
+            display.value += value;
+        })
+    })
+
+    equal.addEventListener('click', function(e){
+        if(display.value === '') {
+            display.value = ""
+        } else {
+            let answer = eval(display.value);
+            display.value = answer
+        }
+    })
+
+    clear.addEventListener('click', function(e){
+        display.value = ""
+    })
+
+})()
